4f1bed2366330bb4067f4be0e2954737c0ee6407
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-11-21 17:07:31 +0100

    Add algolia search

--- .eleventy.js
+++ .eleventy.js
@@ -76,6 +76,22 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('img')
   eleventyConfig.addPassthroughCopy('css')
 
+  eleventyConfig.addPassthroughCopy('js')
+
+  eleventyConfig.addFilter('jsonify', function (variable) {
+    return JSON.stringify(variable)
+  })
+
+  // Remove <code>.*</code>, remove HTML, then with plain text, limit to 5k chars
+  eleventyConfig.addFilter('algExcerpt', function (text) {
+    //first remove code
+    text = text.replace(/<code class="language-.*?">.*?<\/code>/gs, '')
+    //now remove html tags
+    text = text.replace(/<.*?>/g, '')
+    //now limit to 5k
+    return text.substring(0, 5000)
+  })
+
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
     html: true,
---
+++ .functions/deploy-succeeded/deploy-succeeded.js
@@ -0,0 +1,37 @@
+const indexing = require('algolia-indexing')
+const algCredentials = {
+  appId: process.env.ALG_APP_ID,
+  apiKey: process.env.ALG_API_KEY,
+  indexName: 'eleventy_test',
+}
+
+const fetch = require('node-fetch')
+
+exports.handler = async (event, context) => {
+  try {
+    /// HANDLE ALOGLIA
+    // first, get my index
+    let dataResp = await fetch('https://tldreact.dev/algolia.json')
+
+    let data = await dataResp.json()
+    console.log(
+      'Successfully got the data, size of articles ' + data.length,
+      data[0].title,
+    )
+
+    indexing.verbose()
+
+    const settings = {}
+    try {
+      await indexing.fullAtomic(algCredentials, data, settings)
+    } catch (e) {
+      console.log('error in fullAtomic', e)
+    }
+    console.log('Algolia indexing updated. Hopefully.')
+
+    return {statusCode: 200, body: "I'm done with this shit..."}
+  } catch (err) {
+    console.log('error handler for function ran', err.toString())
+    return {statusCode: 500, body: err.toString()}
+  }
+}
