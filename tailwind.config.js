d6bc589209a4fb0e3cca9dc64baa28ffc38d1904
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-11-30 17:12:44 +0100

    fix(style): enable layers purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 module.exports = {
   purge: {
+    enabled: true,
     content: ["./**/*.js", "./**/*.11ty.js", "./**/*.html", "./**/*.njk"],
   },
   theme: {},
   variants: {},
   plugins: [require("@tailwindcss/typography")],
+  future: {
+    purgeLayersByDefault: true,
+  },
 };
