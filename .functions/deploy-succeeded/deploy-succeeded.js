cc96ab9df51cfbe116fdf6554f6843729a077011
Khaled Garbaza <kgarbaya@Wissems-MacBook-Pro-2.fritz.box>
2020-12-05 21:24:39 +0100

    fix: move algolia update to a build time script instead of lambda

--- .functions/deploy-succeeded/deploy-succeeded.js
+++ .functions/deploy-succeeded/deploy-succeeded.js
@@ -1,35 +1,35 @@
-const indexing = require('algolia-indexing')
-const algCredentials = {
-  appId: process.env.ALG_APP_ID,
-  apiKey: process.env.ALG_API_KEY,
-  indexName: 'tldreact',
-}
+// const indexing = require('algolia-indexing')
+// const algCredentials = {
+//   appId: process.env.ALG_APP_ID,
+//   apiKey: process.env.ALG_API_KEY,
+//   indexName: 'tldreact',
+// }
 
-const fetch = require('node-fetch')
+// const fetch = require('node-fetch')
 
-exports.handler = async (event, context) => {
-  try {
-    let dataResp = await fetch('https://tldreact.dev/algolia.json')
+// exports.handler = async (event, context) => {
+//   try {
+//     let dataResp = await fetch('https://tldreact.dev/algolia.json')
 
-    let data = await dataResp.json()
-    console.log(
-      'Successfully got the data, size of articles ' + data.length,
-      data[0].title,
-    )
+//     let data = await dataResp.json()
+//     console.log(
+//       'Successfully got the data, size of articles ' + data.length,
+//       data[0].title,
+//     )
 
-    indexing.verbose()
+//     indexing.verbose()
 
-    const settings = {}
-    try {
-      await indexing.fullAtomic(algCredentials, data, settings)
-    } catch (e) {
-      console.log('error in fullAtomic', e)
-    }
-    console.log('Algolia indexing updated. Hopefully.')
+//     const settings = {}
+//     try {
+//       await indexing.fullAtomic(algCredentials, data, settings)
+//     } catch (e) {
+//       console.log('error in fullAtomic', e)
+//     }
+//     console.log('Algolia indexing updated. Hopefully.')
 
-    return {statusCode: 200, body: "I'm done with this shit..."}
-  } catch (err) {
-    console.log('error handler for function ran', err.toString())
-    return {statusCode: 500, body: err.toString()}
-  }
-}
+//     return {statusCode: 200, body: "I'm done with this shit..."}
+//   } catch (err) {
+//     console.log('error handler for function ran', err.toString())
+//     return {statusCode: 500, body: err.toString()}
+//   }
+// }
---
+++ scripts/update-algolia-index.js
@@ -0,0 +1,34 @@
+const indexing = require('algolia-indexing')
+const algCredentials = {
+  appId: process.env.ALG_APP_ID,
+  apiKey: process.env.ALG_API_KEY,
+  indexName: 'tldreact',
+}
+
+let data = require('./_site/algolia.json')
+
+const updateAlgoliaIndex = async (event, context) => {
+  try {
+    console.log(
+      'Successfully got the data, size of articles ' + data.length,
+      data[0].title,
+    )
+
+    indexing.verbose()
+
+    const settings = {}
+    try {
+      await indexing.fullAtomic(algCredentials, data, settings)
+    } catch (e) {
+      console.log('error in fullAtomic', e)
+    }
+    console.log('Algolia indexing updated. Hopefully.')
+
+    return {statusCode: 200, body: "I'm done with this shit..."}
+  } catch (err) {
+    console.log('error handler for function ran', err.toString())
+    return {statusCode: 500, body: err.toString()}
+  }
+}
+
+updateAlgoliaIndex()
